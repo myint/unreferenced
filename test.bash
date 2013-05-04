f2d24c1c06fc4cea81a4c8cbf068b98d1cfad5cf
Steven Myint <git@stevenmyint.com>
2013-05-04 09:08:37 -0700

    Add a minimal test

---
+++ test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+#
+# Minimal test.
+
+rm -rf ./.test
+mkdir -p ./.test
+touch ./.test/unused.txt
+echo 'self_reference' > ./.test/self_reference
+touch ./.test/okay.txt
+echo 'okay.txt' > ./.test/referrer.txt
+[ $(./unreferenced ./.test | wc -l) -eq 3 ]
+rm -rf ./.test
