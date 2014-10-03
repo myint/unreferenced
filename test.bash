207af58932834132365e8674a6e51c70aeaff427
Steven Myint <git@stevenmyint.com>
2014-10-02 18:44:09 -0700

    Use lowercase

--- test.bash
+++ test.bash
@@ -10,8 +10,8 @@ touch ./.test/unused.txt
 echo 'self_reference' > ./.test/self_reference
 touch ./.test/okay.txt
 echo 'okay.txt' > ./.test/referrer.txt
-readonly LINES=$(./unreferenced ./.test | wc -l)
-[ "$LINES" -eq 3 ]
+readonly lines=$(./unreferenced ./.test | wc -l)
+[ "$lines" -eq 3 ]
 rm -rf ./.test
 
 echo -e '\x1b[01;32mOkay\x1b[0m'
